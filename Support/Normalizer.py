ba098fd0bfe22277e64fb1075af68796e38e4390
Sharad Bhat <sharad.mbhat@gmail.com>
2017-07-10 01:25:13 +0530

    Update Normalizer.py

--- Support/Normalizer.py
+++ Support/Normalizer.py
@@ -1,19 +1,19 @@
 import csv
 
-ratingsFileName = 'ratings.csv'
+ratings_filename = 'ratings.csv'
 normal = 'normalizedratings.csv'
 file2 = open(ratingsFileName, "rt", encoding="utf-8")
 file3 = open(normal, "wt", encoding="utf-8", newline='')
-ratingReader = csv.reader(file2)
+rating_reader = csv.reader(file2)
 
-ratingWriter = csv.writer(file3)
+rating_writer = csv.writer(file3)
 
 file2.seek(0)
 avg = [0]
 for user in range(1, 672):
     total = 0
     n = 0
-    for row in ratingReader:
+    for row in rating_reader:
         if str(row[0]) == str(user):
             total += float(row[2])
             n = n + 1
@@ -25,8 +25,8 @@ for user in range(1, 672):
 file2.seek(0)
 file3.seek(0)
 for user in range(1, 672):
-    for row in ratingReader:
+    for row in rating_reader:
         if str(row[0]) == str(user):
-            ratingWriter.writerow((user, row[1], row[2], str(float(row[2]) - avg[user])))
+            rating_writer.writerow((user, row[1], row[2], str(float(row[2]) - avg[user])))
         else:
             break
