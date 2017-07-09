5b5ae69e39696e1add19a14a293fec7bc281b66d
Sharad Bhat <sharad.mbhat@gmail.com>
2017-07-10 01:11:18 +0530

    CamelCase to snake_case

--- RecommenderEngine2.0.py
+++ RecommenderEngine2.0.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, redirect, url_for, jsonify
 import csv
-import requests
 import numpy as np
 import yaml
 import ast
@@ -8,34 +7,34 @@ import ast
 app = Flask(__name__)
 
 #FILES
-moviesFileName = 'Movies.csv'
-genreCountFileName = 'GenreCount.csv'
-normalizedRatingDictionaryFileName = 'NormalizedRatingDictionary.csv'
-storedURLFileName = 'URL.csv'
+movies_filename = 'Movies.csv'
+genre_count_filename = 'GenreCount.csv'
+normalized_rating_dictionary_filename = 'NormalizedRatingDictionary.csv'
+stored_URL_filename = 'URL.csv'
 
-file1 = open(moviesFileName, "rt", encoding="utf-8")
-movieReader = csv.reader(file1)
+file1 = open(movies_filename, "rt", encoding="utf-8")
+movie_reader = csv.reader(file1)
 
-file2 = open(genreCountFileName, "rt", encoding="utf-8")
-genreCountReader = csv.reader(file2)
+file2 = open(genre_count_filename, "rt", encoding="utf-8")
+genre_count_reader = csv.reader(file2)
 
-file3 = open(normalizedRatingDictionaryFileName, "rt", encoding="utf-8")
-normalizedRatingReader = csv.reader(file3)
+file3 = open(normalized_rating_dictionary_filename, "rt", encoding="utf-8")
+normalized_rating_reader = csv.reader(file3)
 
-file4 = open(storedURLFileName, "rt", encoding="utf-8")
-urlReader = csv.reader(file4)
+file4 = open(stored_URL_filename, "rt", encoding="utf-8")
+url_reader = csv.reader(file4)
 
 
 
 #GLOBAL VARIABLES
-userGenreCounter = {'Comedy' : 0, 'Action' : 0, 'Sci-Fi' : 0, 'Drama' : 0, 'Romance' : 0, 'Thriller' : 0, 'Mystery' : 0, 'Horror' : 0, 'Animation' : 0, 'Adventure' : 0}
+user_genre_counter = {'Comedy' : 0, 'Action' : 0, 'Sci-Fi' : 0, 'Drama' : 0, 'Romance' : 0, 'Thriller' : 0, 'Mystery' : 0, 'Horror' : 0, 'Animation' : 0, 'Adventure' : 0}
 #For Content Filtering Method
 
 b = [] #Stores the NP Array of user's genre rating
 
-topUsers = [] #Stores list of similar users in decreasing order of Pearson Coefficient with user
+top_users = [] #Stores list of similar users in decreasing order of Pearson Coefficient with user
 
-recommendedMoviesDict = {} #Stores recommended movies and the IMDb URLs of those movies
+recommended_movies_dict = {} #Stores recommended movies and the IMDb URLs of those movies
 
 
 
@@ -45,12 +44,12 @@ recommendedMoviesDict = {} #Stores recommended movies and the IMDb URLs of those
 @app.route('/selectedmovies', methods=['POST'])
 def GetMovieListAndGenreCount():
 
-    global userSelectedMovies
-    userSelectedMovies = (request.json)['selectedmovieslist']
+    global user_selected_movies
+    user_selected_movies = (request.json)['selectedmovieslist']
 
     file4.seek(0)
 
-    RatingNormalizer(userSelectedMovies)
+    RatingNormalizer(user_selected_movies)
 
     return redirect(url_for('GetRecommendedMovies'))
 
@@ -59,79 +58,79 @@ def GetMovieListAndGenreCount():
 #Normalizes the user's ratings
 #Takes user selected movies as input
 #No output
-def RatingNormalizer(userSelectedMovies):
+def RatingNormalizer(user_selected_movies):
 
-    normalizedMovieRatingsDict = {}
+    normalized_movies_rating_dict = {}
     total = 0
     count = 0
 
-    for movie in userSelectedMovies:
-        total += userSelectedMovies[movie]
+    for movie in user_selected_movies:
+        total += user_selected_movies[movie]
         count = count + 1
     average = total / count
 
-    for movie in userSelectedMovies:
-        normalizedMovieRatingsDict.update({str(movie) : userSelectedMovies[movie] - average})
+    for movie in user_selected_movies:
+        normalized_movies_rating_dict.update({str(movie) : user_selected_movies[movie] - average})
 
-    GenreCounterUpdater(normalizedMovieRatingsDict)
+    GenreCounterUpdater(normalized_movies_rating_dict)
 
 
 
 #Updates the genre count based on selected movies and ratings
 #Takes selected movies and rating dictionary as input
 #No output
-def GenreCounterUpdater(normalizedMovieRatingsDict):
+def GenreCounterUpdater(normalized_movies_rating_dict):
 
-    global userGenreCounter
-    for genre in userGenreCounter:
-        userGenreCounter[genre] = 0 #Resetting all genre counts to zero
+    global user_genre_counter
+    for genre in user_genre_counter:
+        user_genre_counter[genre] = 0 #Resetting all genre counts to zero
 
-    normalizedUserGenreRatings = []
+    normalized_user_genre_ratings = []
 
-    for userMovieID in normalizedMovieRatingsDict:
+    for user_movie_ID in normalized_movies_rating_dict:
         file1.seek(0)
-        for row in movieReader:
-            if str(row[0]) == str(userMovieID):
+        for row in movie_reader:
+            if str(row[0]) == str(user_movie_ID):
                 for genre in row[2].split('|'):
-                    if genre in userGenreCounter:
-                        userGenreCounter[genre] += round(normalizedMovieRatingsDict[userMovieID], 2)
+                    if genre in user_genre_counter:
+                        user_genre_counter[genre] += round(normalized_movies_rating_dict[user_movie_ID], 2)
                 break
 
-    for genre in userGenreCounter:
-        normalizedUserGenreRatings.append(userGenreCounter[genre])
+    for genre in user_genre_counter:
+        normalized_user_genre_ratings.append(user_genre_counter[genre])
 
-    SortUsers(normalizedUserGenreRatings)
+    SortUsers(normalized_user_genre_ratings)
 
 
 
 #Sorts the stored users based on the cosine angle between the rating vectors of the stored user and new user
 #No input
 #No output
-def SortUsers(normalizedUserGenreRatings):
+def SortUsers(normalized_user_genre_ratings):
 
-    pearsonCoefficientDict = {}
+    pearson_coefficient_dict = {}
     file2.seek(0)
-    b = np.array(normalizedUserGenreRatings)
+    b = np.array(normalized_user_genre_ratings)
 
-    for row in genreCountReader:
+    for row in genre_count_reader:
         pearCo = PearsonCoefficient(ast.literal_eval(row[1]), b)
-        pearsonCoefficientDict.update({row[0] : pearCo})
+        pearson_coefficient_dict.update({row[0] : pearCo})
 
-    sortedUsers = sorted(pearsonCoefficientDict, key = pearsonCoefficientDict.__getitem__, reverse = True)
-    global topUsers
+    sorted_users = sorted(pearson_coefficient_dict, key = pearson_coefficient_dict.__getitem__, reverse = True)
+    global top_users
     for i in range(0, 5):
-        topUsers.append(sortedUsers[i])
+        top_users.append(sorted_users[i])
 
-    SelectMovies(topUsers)
+    SelectMovies(top_users)
 
 
 
 #Calculates the cosine angle between the stored user's and new user's rating vectors
 #Takes rating lists as input
 #Returns cosine angle between rating vectors
-def PearsonCoefficient(genreCountList, b):
+def PearsonCoefficient(genre_count_list, b):
 
-    a = np.array(genreCountList)
+    a = np.array(genre_count_list)
 
     cosine = (np.dot(a, b)) / ((np.sqrt((a * a).sum())) * np.sqrt((b * b).sum()))
 
@@ -142,40 +141,40 @@ def PearsonCoefficient(genreCountList, b):
 #Generates a list of movies with good ratings between all the most similar users
 #Takes list of top 5 most similar users
 #No output
-def SelectMovies(topUsers):
+def SelectMovies(top_users):
 
-    recommendedMoviesTempDict = {}
+    recommended_movies_temp_dict = {}
 
-    for j in topUsers:
+    for j in top_users:
         file3.seek(0)
-        for row in normalizedRatingReader:
+        for row in normalized_rating_reader:
             if str(row[0]) == str(j):
-                tempDict = yaml.load(row[1])
-                for i in tempDict:
-                    if tempDict[i] > 0:	#If the normalized rating is greater than 0, the user liked it
-                        if i not in recommendedMoviesTempDict:
-                            recommendedMoviesTempDict.update({i : tempDict[i]})
+                temp_dict = yaml.load(row[1])
+                for i in temp_dict:
+                    if temp_dict[i] > 0:	#If the normalized rating is greater than 0, the user liked it
+                        if i not in recommended_movies_temp_dict:
+                            recommended_movies_temp_dict.update({i : temp_dict[i]})
 
-    global recommendedMoviesList
-    recommendedMoviesList = sorted(recommendedMoviesTempDict, key = recommendedMoviesTempDict.__getitem__, reverse = True)
+    global recommended_movies_list
+    recommended_movies_list = sorted(recommended_movies_temp_dict, key = recommended_movies_temp_dict.__getitem__, reverse = True)
 
-    global recommendedMoviesDict
+    global recommended_movies_dict
     for i in range(0, 15):
-        url = GetUrl(recommendedMoviesList[i])
-        recommendedMoviesDict.update({recommendedMoviesList[i] : url})
+        url = GetUrl(recommended_movies_list[i])
+        recommended_movies_dict.update({recommended_movies_list[i] : url})
 
 
 
 #Returns the IMDb or Rotten Tomatoes URL of the movie
 #Takes movie ID as input
 #Returns URL of the movie
-def GetUrl(movieID):
+def GetUrl(movie_ID):
 
     #file4.seek(0)
     #As the Movie IDs are in ascending order, there is no need to reset the file reader to starting
     url = ""
-    for row4 in urlReader:
-        if str(movieID) == str(row4[0]):
+    for row4 in url_reader:
+        if str(movie_ID) == str(row4[0]):
             url = str(row4[1])
             break
     return url
@@ -188,7 +187,7 @@ def GetUrl(movieID):
 @app.route('/recommended', methods=['GET'])
 def GetRecommendedMovies():
 
-    return jsonify(recommendedMoviesDict)
+    return jsonify(recommended_movies_dict)
 
 
 
