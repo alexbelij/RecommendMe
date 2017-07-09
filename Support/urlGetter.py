3759a62d478a61d11bfdfc9557aaa84ae3dd0ad3
Sharad Bhat <sharad.mbhat@gmail.com>
2017-07-10 01:24:01 +0530

    Update urlGetter.py

--- Support/urlGetter.py
+++ Support/urlGetter.py
@@ -6,22 +6,22 @@ import requests
 
 file2 = open("Movies.csv", "rt", encoding="utf-8")
 file3 = open("URL.csv", "wt", encoding="utf-8", newline='')
-movieReader = csv.reader(file2)
+movie_reader = csv.reader(file2)
 
-URLWriter = csv.writer(file3)
+URL_writer = csv.writer(file3)
 
 file2.seek(0)
-for row in movieReader:
+for row in movie_reader:
     try:
-        movieSearchTerm = row[1] + " imdb"
-        SearchUrl = r'https://www.google.co.in/search?q=' + urllib.parse.quote_plus(movieSearchTerm)
-        searchResult = requests.get(SearchUrl)
-        searchResult.raise_for_status()
-        soup = bs4.BeautifulSoup(searchResult.text, 'html.parser')
-        IMDbID = str(soup.find('cite'))[32:41]
-        url = "http://www.imdb.com/title/" + IMDbID
-        URLWriter.writerow((row[0], url))
+        movie_search_term = row[1] + " imdb"
+        earch_url = r'https://www.google.co.in/search?q=' + urllib.parse.quote_plus(movieSearchTerm)
+        search_result = requests.get(search_url)
+        search_result.raise_for_status()
+        soup = bs4.BeautifulSoup(search_result.text, 'html.parser')
+        imdb_ID = str(soup.find('cite'))[32:41]
+        url = "http://www.imdb.com/title/" + imdb_ID
+        URL_writer.writerow((row[0], url))
         print(str(row[0]) + " done")
     except:
         print(str(row[0]) + "error")
-        URLWriter.writerow((row[0], "Error"))
+        URL_writer.writerow((row[0], "Error"))
